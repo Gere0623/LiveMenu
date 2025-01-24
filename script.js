b6df512c0842275b114c0ee6701a4c269d5255cc
Geremia <geremianatiello@gmail.com>
2025-01-24 22:28:24 +0100

    collegamento tra mockup menu statico e logica 3D

--- script.js
+++ script.js
@@ -1,19 +1,28 @@
-var elem_lista = `<div class="row m-0 my-2 elemento">
-            <div class="col rounded bg-light py-2 px-2">
-                <div class="row p-2">
-                    <div class="col-4 d-flex justify-content-center">
-                        <img class="img-fluid" src="img/hamburger.png" alt="hamburger">
-                    </div>
-                    <div class="col d-flex justify-content-center flex-column">
-                        <h1>hamburger</h1>
-                        <p>è un hamburger -_-</p>
-                    </div>
-                </div>
-            </div>
-        </div>`; 
 
-function popolaLista(){
-    for(let i = 0; i < 10; i++)
+
+
+
+
+// !!! temporaneo
+
+var elem_lista = `<a href="#">
+                    <div class="row m-0 my-2">
+                        <div class="col rounded bg-light py-2 px-2">
+                            <div class="row p-2">
+                                <div class="col-4 d-flex justify-content-center">
+                                    <img class="img-fluid" src="img/hamburger.png" alt="hamburger">
+                                </div>
+                                <div class="col d-flex justify-content-center flex-column">
+                                    <h1>Hamburger</h1>
+                                    <p>è un Hamburger</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </a>`;
+
+function popolaLista() {
+    for (let i = 0; i < 10; i++)
         document.getElementById("lista").innerHTML += elem_lista;
 }
 
